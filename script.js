7ac05f984d2d1142c74fd646e59e816cfc710581
Fitridwianisa <158561694+Fitridwianisa@users.noreply.github.com>
2025-04-07 08:22:33 +0700

    Update script.js
    Ubah url whatsaap menjadi email

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
     const name = this.name.value;
     const subject = this.subject.value;
     const message = this.message.value;
-  
-    const whatsappURL = `https://wa.me/6285748196884?text=Halo%20Fitri!%0ASaya%20${name}%0ASubjek:%20${subject}%0APesan:%20${message}`;
-    window.open(whatsappURL, '_blank');
+    
+    const email = 'sablahnisa201@gmail.com';
+    const mailtoURL = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=Halo Fitri!%0ASaya ${encodeURIComponent(name)}%0A${encodeURIComponent(message)}`;
+    
+    window.open(mailtoURL, '_blank');
   });
   
